9cb41043ac781df6ee0e888a6445d292150aca13
Veysel ÖREN <orenveysel@outlook.com>
2021-12-29 21:57:31 +0300

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,16 @@
+name = "Ebubekir"
+surname = 'Sıddık'
+age = 1
+
+greeting = "My name is "+ name + " "+ surname + " and \nI'm "+ str(age) + " years old"
+length = len(greeting)
+
+# print(greeting)
+# print(greeting[0])
+# print(greeting[3])
+# print(greeting[length-1])
+# print(greeting[-1])
+# print(greeting[3:7])
+# print(greeting[3:])
+# print(greeting[:19])
+# print(greeting[2:40:3])
